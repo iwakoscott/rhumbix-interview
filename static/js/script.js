c4fa169598b3ad23f9800bed67644c7d6ca440fb
Satoshi Scott Iwako <iwakoscott@gmail.com>
2017-10-05 17:52:51 -0700

    add basic autocomplete

--- static/js/script.js
+++ static/js/script.js
@@ -10,15 +10,43 @@ giphyViewModel = function(){
     // format url with searchTerm
     var url = 'http://api.giphy.com/v1/gifs/search?q=' + self.searchTerm() + '&api_key=dc6zaTOxFJmzC&limit=1';
 
+
+    // get JSON from aforementioned URL
     $.getJSON(url, function(json){
       var gifUrl = json.data[0].embed_url;
       $('.gif-display').attr({'src': gifUrl});
     });
 
 
-
-
   }
+
 }
 
 ko.applyBindings(new giphyViewModel());
+
+
+$(function(){
+
+  var availableTags = [
+    'about',
+    'above',
+    'across',
+    'app',
+    'apple',
+    'appreciate',
+    'bad',
+    'ball',
+    'balloon',
+    'bell',
+    'cat'
+  ]
+
+  $('#search').autocomplete({
+    source: availableTags,
+    messages: {
+        noResults: '',
+        results: function() {}
+    }
+  });
+
+});
